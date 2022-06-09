244b5c967ba7233230511188109303025e91f36a
Justin Bush <revblaze@users.noreply.github.com>
2022-06-09 14:16:13 -0600

    Remove data/cache/

--- data/cache/9/9cadeb6f61083dcd27b03d8256c2536a.js
+++
@@ -1,1 +0,0 @@
-SimpleSVG._loaderCallback({"prefix":"mdi","icons":{"download":{"body":"<path d=\"M5 20h14v-2H5m14-9h-4V3H9v6H5l7 7l7-7z\" fill=\"currentColor\"\/>"}},"aliases":[],"width":24,"height":24});
